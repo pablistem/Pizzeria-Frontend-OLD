73e130ea1a8c1635c6ada4a219e60388326978f2
Fernando Gaspari <dev.gaspari.fernando@gmail.com>
2023-02-10 01:05:25 -0300

    merge/48-49-94: fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './component/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { MenuComponent } from './pages/menu/menu.component';
