b02fb75cc8ef291a384710c3c5f79a8dec1211bb
MatiasEsp404 <88750230+MatiasEsp404@users.noreply.github.com>
2023-03-19 19:32:32 -0300

    I removed the auto-login after registration

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from 'src/app/service/auth.service';
-import { switchMap } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { RegisterRequest } from 'src/app/model/user';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -43,44 +44,31 @@ export class RegisterComponent {
    * Si el logue es exitoso, redirige al usuario a la página principal.
    */
   onSubmit() {
-    console.log(this.formGroup.value);
-
     // Compruebo si el formulario es válido. Si no lo es, se detiene la ejecución del método.
     if (this.formGroup.invalid) {
       return;
     }
 
     // Obtengo los valores de los inputs del formulario
-    const name = this.formGroup.controls['nombre'].value;
-    const lastName = this.formGroup.controls['apellido'].value;
-    const email = this.formGroup.controls['email'].value;
-    const password = this.formGroup.controls['password'].value;
+    const request : RegisterRequest = {
+      name: this.formGroup.controls['nombre'].value,
+      lastName: this.formGroup.controls['apellido'].value,
+      email: this.formGroup.controls['email'].value,
+      password: this.formGroup.controls['password'].value
+    };
 
-    // LLamo a AuthService para registrar y loguear al usuario.
-    this.authService.register({
-      name: name,
-      lastName: lastName,
-      email: email,
-      password: password
-    }).pipe(
-      // Utilizo switchMap de rxjs para evitar el callback hell al anidar requests
-      switchMap(() =>
-        // Logueo al usuario recién registrado
-        this.authService.login({ email: email, password: password })
-      ))
-      .subscribe({
-        next: (userData) => {
-          console.log(userData);
-          this.serverError = false;
-          // Redirige al usuario al Home
-          this.router.navigateByUrl('/home');
-        },
-        // Error en el servidor backend
-        error: (errorMsg) => {
-          console.error(errorMsg);
-          this.serverError = true;
-        }
-      })
+    // Si hay que redirigir al usuario a alguna página:
+    // this.router.navigateByUrl('/home');
+    this.authService.register(request).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.serverError = false;
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error.error.msg);
+        this.serverError = true;
+      }
+    });
   }
 
 }
