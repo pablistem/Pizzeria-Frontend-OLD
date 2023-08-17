d918d1babb93217c7c6cf882570ab64f15a138db
MatiasEsp404 <unity.matias@outlook.com>
2023-08-16 22:21:01 -0300

    Correccion de errores en el menu

--- src/app/component/home/detalles/detalles.component.ts
+++ src/app/component/home/detalles/detalles.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { Product } from 'src/app/model/product';
 
 @Component({
@@ -11,6 +11,16 @@ export class DetallesComponent {
   @Output() eventoVolver: EventEmitter<void> = new EventEmitter();
   @Input() producto: Product | undefined;
   cantidad: number = 1;
+  mostrarBtnVolver: boolean = false;
+
+  constructor() {
+    this.mostrarBtnVolver = window.innerWidth < 1000;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: any): void {
+    this.mostrarBtnVolver = window.innerWidth < 1000;
+  }
 
   menos() {
     if (this.cantidad != 1) {
